b570016aa8c0f845dc322a0b8952cfcdef5be7c5
Ankita Kurle <ankitakurle39@gmail.com>
2025-12-15 17:25:36 +0530

    Fix Vercel API route regex

--- serverr.js
+++ serverr.js
@@ -5,10 +5,17 @@ import "dotenv/config";
 import chatRoutes from "./routes/chat.js";
 
 const app=express();
-const PORT=8080;
+const PORT=process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+
+app.use((req, res, next) => {
+    if (req.path === "/") {
+        return res.redirect("/api");
+    }
+    next();
+});
 
 app.use("/api",chatRoutes);
 
